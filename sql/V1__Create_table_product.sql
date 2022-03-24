1ce894e507c4651c1b33460a62fd784e3e9bf3d9
ragh01 <65010097+ragh01@users.noreply.github.com>
2022-03-24 11:37:44 +0530

    Update V1__Create_table_product.sql

--- sql/V1__Create_table_product.sql
+++ sql/V1__Create_table_product.sql
@@ -1,6 +1,6 @@
 create table test(
-id integer, -- auto incrementing IDs   
-name varchar(100),  -- variable string column
-preferences string, -- column used to store JSON type of data
-created_at timestamp
+  id integer, -- auto incrementing IDs   
+  name varchar(100),  -- variable string column
+  preferences string, -- column used to store JSON type of data
+  created_at timestamp
 );
