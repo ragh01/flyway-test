f636671ecc2de953024144ba357b5714d7d81f2b
Raghav Mittal <rmittal@phdata.io>
2022-03-24 14:43:46 +0530

    flyway testing

---
+++ sql/V4__Create_table_test.sql
@@ -0,0 +1,6 @@
+create table temp(
+  id integer, -- auto incrementing IDs   
+  name varchar(100),  -- variable string column
+  preferences varchar(100), -- column used to store JSON type of data
+  created_at timestamp
+);
