8c7012f41e183156fcaf184c2b7d73d5ed84c9e6
Raghav Mittal <rmittal@phdata.io>
2022-03-28 10:47:14 +0530

    multiple table addition

---
+++ sql/V6__Add_two_tables.sql
@@ -0,0 +1,20 @@
+create table abc1(
+  id integer, -- auto incrementing IDs   
+  name varchar(100),  -- variable string column
+  preferences varchar(100), -- column used to store JSON type of data
+  created_at timestamp
+);
+
+create table abc2(
+  id integer, -- auto incrementing IDs   
+  name varchar(100),  -- variable string column
+  preferences varchar(100), -- column used to store JSON type of data
+  created_at timestamp
+);
+
+create table abc3(
+  id integer, -- auto incrementing IDs   
+  name varchar(100),  -- variable string column
+  preferences varchar(100), -- column used to store JSON type of data
+  created_at timestamp
+);
